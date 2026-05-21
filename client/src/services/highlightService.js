d070895fecea7730c6d43727751d830d303410e8
awan ibrahim <awanibrahim200182@gmail.com>
2026-05-21 17:09:18 +0530

    frontend deployment setup

--- client/src/services/highlightService.js
+++ client/src/services/highlightService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/highlights";
+const API_URL = "https://cricket-dashboard-q54o.onrender.com/api/highlights";
 
 export const highlightService = {
   getHighlights: async () => {
--- client/src/services/newsService.js
+++ client/src/services/newsService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/news";
+const API_URL = "https://cricket-dashboard-q54o.onrender.com/api/news";
 
 export const newsService = {
   getNews: async () => {
